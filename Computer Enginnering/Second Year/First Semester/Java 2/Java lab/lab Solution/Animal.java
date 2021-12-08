c7f624343e07dc994b2ea96b8b9dc3eb57ca2b2c
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2021-12-08 22:07:50 +0200

    Update Animal.java

--- Computer Enginnering/Second Year/First Semester/Java 2/Java lab/lab Solution/Animal.java
+++ Computer Enginnering/Second Year/First Semester/Java 2/Java lab/lab Solution/Animal.java
@@ -1,12 +1,17 @@
 public abstract class Animal {
+  
 protected int legs;
+  
 protected Animal(int legs)
 {
 this.legs=legs;
 }
+  
 public void walk()
 {
 System.out.println("This animal walk by "+legs+" legs.");
 }
+  
 public abstract void eat();
+  
 }
