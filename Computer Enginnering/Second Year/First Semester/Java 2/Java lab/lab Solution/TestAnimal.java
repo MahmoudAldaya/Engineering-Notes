7383278887f673b2c4a21cdb008add49db83bfea
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2021-12-08 22:06:32 +0200

    Create TestAnimal.java

---
+++ Computer Enginnering/Second Year/First Semester/Java 2/Java lab/lab Solution/TestAnimal.java
@@ -0,0 +1,34 @@
+public class TestAnimals {
+  
+public static void main(String[] args) {
+  
+Fish d = new Fish();
+Cat c = new Cat("Fluffy");
+Animal a = new Fish();
+Animal e = new Spider();
+Pet p = new Cat();
+  
+d.setName(" ");//Enter cat name
+d.eat();
+System.out.println("This fish's name is"+d.getName());
+d.walk();
+  
+c.eat();
+System.out.println("This cat's name is"+c.getName());
+c.walk();
+  
+((Fish)a).setName(" ");//Enter fish name
+((Fish)a).eat();
+System.out.println("This fish's name is"+((Fish)a).getName());
+((Fish)a).walk();
+  
+((Spider)e).eat();
+((Spider)e).walk();
+  
+((Cat)p).setName(" ");//Enter cat name
+((Cat)p).eat(); 
+System.out.println("This Cat's name is"+((Cat)p).getName());
+((Cat)p).walk();
+  
+}
+}
