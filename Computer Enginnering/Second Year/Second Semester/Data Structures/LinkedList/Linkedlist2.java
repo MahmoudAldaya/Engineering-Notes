03b3f94ca4b26862d0463cf11ed62fe4dc2578e6
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-02-22 20:51:10 +0200

    Create Linkedlist2.java

---
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/LinkedList/Linkedlist2.java
@@ -0,0 +1,12 @@
+public class LinkedList2 {
+    public static void main(String[] args) {
+        LList l1 = new LList();
+        l1.addfirst(10);
+        l1.addfirst(20);
+        l1.addfirst(30);
+        l1.addlast(5);
+        l1.addlast(1);
+
+        l1.print();
+    }
+}
