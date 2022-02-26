51fe2ff0e3eb7d39ea6dd4d152bb4987d46080d5
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-02-26 22:10:21 +0200

    Create LinkedList22.java

---
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/LinkedList/LinkedList22.java
@@ -0,0 +1,27 @@
+public class LinkedList2 {
+    public static void main(String[] args) {
+        LList l1 = new LList();
+        LList l2 = new LList();
+        
+        l1.addfirst(10);
+        l1.addfirst(20);
+        l1.addfirst(30);
+        l1.addlast(5);
+        l1.addlast(1);
+
+// Homeworks && 26/2 Lecture: removeLast, removeFirst, addBetween_Values, addBetween_Positions, Size, IsEmpty;
+
+         l1.removeLast();
+         l1.removeFirst();
+
+         l1.addBetween_Position(15,2);
+         l1.addBetween_Values(20, 5);
+
+         System.out.println("Is l2 Empty? "+l2.isEmpty()); // Note That L2 is already Empty.
+         System.out.println("Is l1 Empty? "+l1.isEmpty()); // Note That L2 is already NOT Empty.
+         System.out.println("l1 Size: "+l1.size());
+
+         l1.print();
+
+    }
+}
