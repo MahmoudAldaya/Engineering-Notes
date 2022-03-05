7837757fcb560df360c47d63ca0422dca4f7d8a8
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-03-05 18:31:50 +0200

    Create Node class.java

---
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Doubly Linkedlist/Node class.java
@@ -0,0 +1,12 @@
+public class Node3 {
+    Node3 next;
+    Node3 prev;
+    int item;
+
+    public Node3(int item) {
+        this.next = null;
+        this.prev = null;
+        this.item = item;
+
+    }
+}
