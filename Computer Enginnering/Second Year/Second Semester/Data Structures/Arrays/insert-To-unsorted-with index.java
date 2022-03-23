f4612471b1e26dfa36883c28bde7e305309ac3d7
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-03-23 17:55:01 +0200

    Update insert-To-unsorted-with index.java

--- Computer Enginnering/Second Year/Second Semester/Data Structures/Arrays/insert-To-unsorted-with index.java
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Arrays/insert-To-unsorted-with index.java
@@ -1,4 +1,5 @@
 public void insertValue(int[] arr, int value, int index){
-for(int i = arr.length -1 ; i > index ; i--)
-arr[i] = arr[i-1]
-arr[index] = value;
+      for(int i = arr.length -1 ; i > index ; i--)
+          arr[i] = arr[i-1]
+          arr[index] = value;
+}
