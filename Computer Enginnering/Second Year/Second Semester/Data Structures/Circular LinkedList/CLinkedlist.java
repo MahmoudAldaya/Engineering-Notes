d171319c06d38bf3588ba8b7b8e02e28497c250b
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-03-27 16:42:42 +0300

    Create CLinkedlist.java

---
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Circular LinkedList/CLinkedlist.java
@@ -0,0 +1,26 @@
+public class CLinkedlist4 {
+    public static void main(String[] args) {
+        CLList c1 = new CLList();
+        c1.Addfirst("1");
+        c1.Addfirst("2");
+        c1.Addfirst("3");
+       // c1.Addfirst("4");
+       // c1.Addfirst("5");
+       // c1.Addfirst("6");
+       // c1.Addfirst("7");
+       // c1.Addlast("8");
+
+       //c1.Removefirst();
+       // c1.Removefirst();
+       // c1.Removelast();
+
+       // c1.AddBetween("20",2);
+       // c1.Removebetween(2);
+
+        c1.print();
+        //System.out.println();
+       // System.out.println(c1.size());
+
+
+    }
+}
