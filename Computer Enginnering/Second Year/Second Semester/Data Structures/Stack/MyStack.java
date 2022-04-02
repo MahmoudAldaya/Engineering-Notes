1e70d33f4b7a852279d8b48c696d633428912cc0
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-04-02 18:57:01 +0300

    Create MyStack.java

---
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Stack/MyStack.java
@@ -0,0 +1,53 @@
+public class myStack {
+        int [] arr;
+        int top;
+        int capacity;
+
+    public myStack(int size) {
+        arr = new int[size];
+        top = -1;
+        capacity = 0;
+    }
+
+    public void push(int item) { // اضافةقيمة جديدة للستاك
+        if (!IsFull()) {
+            top++;
+            arr[top] = item;
+            capacity++;
+        } else
+            System.out.println("The stack is full!!");
+    }
+
+    public boolean IsEmpty(){ // لفحص هل الستاك فارغ ام لا
+        return (top == -1);
+
+    }
+
+    public boolean IsFull(){ // لفحص هب الستاك ممتلئ ام لا
+        return capacity == arr.length;
+    }
+
+    public int pop(){ // تحذف آخر قيمة اضيفت للستاك
+     int item = 0;
+     if (IsEmpty())
+         System.out.println("stack is empty");
+     else{
+         item = arr[top--];
+     }
+         return item;
+    }
+
+    public void top(){ // بترجع آخر قيمة أضيفت للستاك
+        System.out.println(top);
+    }
+
+    public int size(){ // حجم الستاك
+        return capacity;
+    }
+
+    public void print(){ // لطباعة قيم الستاك
+        for (int i = top; i >= 0; i--) {
+            System.out.println(arr[i]);
+        }
+    }
+}
