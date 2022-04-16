0f7ad5bb40d50f6529cdbc867c69b35f7a8af1cb
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-04-17 01:50:24 +0300

    Create Node class.java

---
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Trees/Node class.java
@@ -0,0 +1,11 @@
+ public class Node4 {
+    Node4 left;
+    Node4 right;
+    int data;
+
+   public Node4(int data) {
+        this.left = null;
+        this.right = null;
+        this.data = data;
+    }
+}
