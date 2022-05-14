9bede70f4db7c10005eeef70da112cbcfb876259
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-05-14 23:49:21 +0300

    Create Insertion_Sort.java

---
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Sorting Algorithims/Insertion_Sort.java
@@ -0,0 +1,12 @@
+  public static int[] insertion_sort (int arr[]){
+        for(int i = 1; i < arr.length ; i++){
+            int key = arr[i];
+            int j = i-1;
+            while(j >= 0 && arr[j] > key){
+                arr[j + 1] = arr[j];
+                j = j - 1;
+            }
+            arr[j + 1] = key;
+        }
+        return arr;
+    }
