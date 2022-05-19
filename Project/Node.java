16c94b998f3ed9fec71a192b296c054c4b23c979
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-05-19 03:18:17 +0300

    Create Node.java

---
+++ Project/Node.java
@@ -0,0 +1,14 @@
+
+package javaapplication_dsproject;
+
+
+public class Node {
+    Book book;
+    Node next;
+
+    public Node(Book book) {
+        this.book = book;
+        this.next = null;
+    }
+ 
+}
