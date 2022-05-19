cd14fa4176a32e49371549466f6a7871149ecbb4
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-05-19 03:17:51 +0300

    Create Book.java

---
+++ Project/Book.java
@@ -0,0 +1,14 @@
+
+package javaapplication_dsproject;
+
+
+public class Book {
+    int book_num;
+    String book_name;
+
+    public Book(int book_num, String book_name) {
+        this.book_num = book_num;
+        this.book_name = book_name;
+    }
+  
+}
