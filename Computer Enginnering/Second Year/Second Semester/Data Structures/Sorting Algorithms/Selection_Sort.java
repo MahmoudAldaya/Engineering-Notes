e6cff5dd37ea6ceee0e8c7a6686081999a91f438
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-06-06 06:04:12 +0300

    Update Selection_Sort.java

--- Computer Enginnering/Second Year/Second Semester/Data Structures/Sorting Algorithms/Selection_Sort.java
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Sorting Algorithms/Selection_Sort.java
@@ -1,12 +1,12 @@
     public static int[] selection_sort(int arr[]){
-        for(int i =0; i< (arr.length)-1; i++){
+        for(int i = 0; i < (arr.length)-1; i++){
             int min = i;
-            for(int j=i+1; j< arr.length; j++){
+            for(int j = i+1; j < arr.length; j++){
                 if(arr[j] < arr [min])
                      min = j;
                 int temp = arr[min];
-                arr[min]=arr[i];
-                arr[i]=temp;
+                arr[min] = arr[i];
+                arr[i] = temp;
             }
         }
         return arr;
