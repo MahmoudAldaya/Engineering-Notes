ea468316cd92b55601742a9e935b2fff9ea7691f
Mahmoud Aldaya <mahmoud1432002@gmail.com>
2022-06-06 06:03:20 +0300

    Update Selection_Sort.java

--- Computer Enginnering/Second Year/Second Semester/Data Structures/Sorting Algorithms/Selection_Sort.java
+++ Computer Enginnering/Second Year/Second Semester/Data Structures/Sorting Algorithms/Selection_Sort.java
@@ -2,7 +2,8 @@
         for(int i =0; i< (arr.length)-1; i++){
             int min = i;
             for(int j=i+1; j< arr.length; j++){
-                min = j;
+                if(arr[j] < arr [min])
+                     min = j;
                 int temp = arr[min];
                 arr[min]=arr[i];
                 arr[i]=temp;
